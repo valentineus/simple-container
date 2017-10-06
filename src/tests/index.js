0b1fbd32c0c2b44cbc494b523c00cb0b9bf8aedd
Valentin Popov <info@valentineus.link>
2017-10-07 00:53:37 +0400

    Changed structure of the test

--- src/tests/index.js
+++ src/tests/index.js
@@ -3,14 +3,27 @@ import { assert } from 'chai';
 import Containers from '../lib/index';
 
 describe('simple-container:', () => {
-    it('simple', (done) => {
+    var container = null;
+
+    before((done) => {
         var containers = new Containers();
         var image = 'hello-world:latest';
 
-        containers.create(image).then(container => {
-            if (container) {
-                done();
-            }
+        containers.create(image).then(item => {
+            container = item;
+            done();
         });
+    });
+
+    after((done) => {
+        container.stop(() => {
+            container.remove(() => {
+                done();
+            });
+        });
+    });
+
+    it('simple', () => {
+        assert.isString(container.id);
     });
 });
