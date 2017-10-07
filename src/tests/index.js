72dfa17df48760efb399e1d6e9c946d8870a2b6b
Valentin Popov <info@valentineus.link>
2017-10-07 17:48:05 +0400

    Full-featured testing

--- src/tests/index.js
+++ src/tests/index.js
@@ -3,27 +3,59 @@ import { assert } from 'chai';
 import Containers from '../lib/index';
 
 describe('simple-container:', () => {
+    var image = 'alpine:latest';
+    var containers = null;
     var container = null;
 
-    before((done) => {
-        var containers = new Containers();
-        var image = 'hello-world:latest';
+    before(() => {
+        containers = new Containers();
+    });
 
-        containers.create(image).then(item => {
+    after((done) => {
+        /* Removing the container after testing */
+        container.remove().then(() => {
+            done();
+        });
+    });
+
+    it('create(image)', (done) => {
+        containers.create(image).then((item) => {
+            assert.isObject(item);
             container = item;
             done();
         });
     });
 
-    after((done) => {
-        container.stop(() => {
-            container.remove(() => {
-                done();
-            });
+    it('info(id)', (done) => {
+        containers.info(container.id).then((item) => {
+            assert.equal(item.Id, container.id);
+            assert.equal(item.Image, image);
+            assert.isObject(item);
+            done();
         });
     });
 
-    it('simple', () => {
-        assert.isString(container.id);
+    it('info()', (done) => {
+        containers.info().then((list) => {
+            var item = list[container.id];
+            assert.equal(item.Id, container.id);
+            assert.equal(item.Image, image);
+            assert.isObject(item);
+            done();
+        });
+    });
+
+    it('get(id)', (done) => {
+        containers.get(container.id).then((item) => {
+            assert.deepEqual(container, item);
+            done();
+        });
+    });
+
+    it('get()', (done) => {
+        containers.get().then((list) => {
+            assert.deepEqual(container, list[container.id]);
+            done();
+        });
     });
 });
